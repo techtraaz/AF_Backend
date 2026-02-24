032a27706584cf0045a69ff7bcf255db056de954
rushxdev <rashmikapabasara@gmail.com>
2026-02-24 11:59:25 +0530

    fix(profile): Add authorizeRoles middleware to profileRoutes

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createProfile, getProfile, updateProfile } from "../controller/profileController.js";
-import { authenticate, authorizeAdmin, authorizeRefugee } from "../middleware/authMiddleware.js";
+import { authenticate, authorizeAdmin, authorizeRefugee, authorizeRoles } from "../middleware/authMiddleware.js";
 import { } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
