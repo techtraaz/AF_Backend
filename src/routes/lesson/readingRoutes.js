3263d9c4b3b4edece3c5263294f13a49b82a9b34
techtraaz <techtraazsl@gmail.com>
2026-02-26 00:30:27 +0530

    feat : added route protection with role based authorization

--- src/routes/lesson/readingRoutes.js
+++ src/routes/lesson/readingRoutes.js
@@ -1,12 +1,13 @@
 
 import express from "express";
 import * as readingController from "../../controller/lesson/readingController.js";
+import { authenticate,authorizeContentContributor} from "../../middleware/authMiddleware.js";
 
 const router = express.Router({ mergeParams: true }); // mergeParams to access lessonId
 
-router.post("/", readingController.createReading);
+router.post("/", authenticate,authorizeContentContributor,readingController.createReading);
 router.get("/", readingController.getReading);
-router.put("/", readingController.updateReading);
-router.delete("/", readingController.deleteReading);
+router.put("/",authenticate,authorizeContentContributor, readingController.updateReading);
+router.delete("/", authenticate,authorizeContentContributor,readingController.deleteReading);
 
 export default router;
